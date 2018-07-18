a6ab8decbdf56e0be53ef7ec412fbf80b5535be4
ninthakeey <ninthakeey@hotmail.com>
2018-07-18 21:53:50 +0800

    vtracer server file upload

---
+++ vtracer-server/README.md
@@ -0,0 +1,8 @@
+# V-Tracer Back End
+
+Tracing member and video info of bilibili website. 
+
+[Demo](http://45.32.68.44:16201)
+
+项目使用springboot进行编写
+
---
+++ vtracer-server/src/main/java/none/nintha/vtracer/util/SpringUtils.java
@@ -0,0 +1,23 @@
+package none.nintha.vtracer.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringUtils implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtils.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(String beanName) {
+        if(applicationContext.containsBean(beanName)){
+            return (T) applicationContext.getBean(beanName);
+        }else{
+            return null;
+        }
+    }
+}
